5b768805285c5c7515728ba2be790a5bd2b750b7
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-04-04 23:43:09 +0200

    Add custom error pages and documentation

--- lettings/models.py
+++ lettings/models.py
@@ -3,8 +3,29 @@ from django.db import models
 
 
 class Address(models.Model):
+    """
+    A Class to represent an address
+
+    ...
+
+    Attributes
+    ----------
+    number: int
+        the house number
+    street: str
+        the street name
+    city: str
+        the city name
+    state: str
+        the state name
+    zip_code: int
+        the zip_code
+    country_iso_code: str
+        the country iso code
+    """
 
     class Meta:
+
         verbose_name_plural = "Addresses"
 
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
@@ -16,13 +37,26 @@ class Address(models.Model):
         max_length=3, validators=[MinLengthValidator(3)]
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.number} {self.street}"
 
 
 class Letting(models.Model):
+    """
+    A class to represent a letting
+
+    ...
+
+    Attributes
+    ----------
+    title: str
+        the title of the letting
+    address: Address
+        the related address for the letting
+    """
+
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,21 @@ from django.db import models
 
 
 class Profile(models.Model):
+    """
+    A class to represent a user's profile
+
+    ...
+
+    Attributes
+    ----------
+    user: User
+        the user linked to the profile
+    favorite_city: str
+        the user's favorite city
+    """
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
-    def __str__(self):
-        return self.user.username
+    def __str__(self) -> str:
+        return str(self.user.username)
