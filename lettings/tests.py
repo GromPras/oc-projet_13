15143fbec3aebc172442d5ca5f051e79ae8c08d6
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-04-07 08:55:33 +0200

    Enable debug and setup testing

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_index_view_use_index_template(client):
+    url = reverse("lettings:index")
+    response = client.get(url)
+    print(response)
+    assertTemplateUsed(response, "lettings/index.html")
