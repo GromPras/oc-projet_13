7a351f8e7b094565b4a91c9e57e7617c222ec9f5
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-04-07 09:30:47 +0200

    Letting tests fixture and test details page

--- lettings/tests.py
+++ lettings/tests.py
@@ -7,5 +7,16 @@ from pytest_django.asserts import assertTemplateUsed
 def test_index_view_use_index_template(client):
     url = reverse("lettings:index")
     response = client.get(url)
-    print(response)
     assertTemplateUsed(response, "lettings/index.html")
+
+
+@pytest.mark.django_db
+def test_letting_is_shown(client, create_address, create_letting):
+    address = create_address()
+    letting = create_letting(address, title="Lovecraft's House")
+    url = reverse("lettings:detail", args=[letting.id])
+    response = client.get(url)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "lettings/letting.html")
+    assert response.context["title"] == letting.title
+    assert response.context["address"] == address
