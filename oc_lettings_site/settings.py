259b383aef18e3cae8279c15d40d6ba385d20de7
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-04-28 10:38:38 +0200

    Add sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import sentry_sdk
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -115,3 +117,11 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    # Add data like request headers and IP for users;
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,9 +3,19 @@ from django.urls import include, path
 
 from . import views
 
+
+def trigger_error(request):
+    """
+    Simple function to test Sentry integration
+    Should be removed after validation
+    """
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
     path("admin/", admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
