800b97a6610e5d763924de7768b488b030e8f7be
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-04-28 11:05:51 +0200

    Move to ENV var setup

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,9 +118,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
+print(f"SENTRY_DSN: {SENTRY_DSN}")
 
 sentry_sdk.init(
-    dsn=os.environ.get("SENTRY_DSN"),
+    dsn=SENTRY_DSN,
     # Add data like request headers and IP for users;
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
