da338dfd6dba6a0a6d604abc57b9178cc925a051
Jérôme Peyras <jerome.peyras@protonmail.com>
2025-06-23 09:32:10 +0200

    Exclude virtual env from linting and remove line too long in settings

--- src/oc_lettings_site/settings.py
+++ src/oc_lettings_site/settings.py
@@ -140,8 +140,6 @@ if SENTRY_DSN:
     try:
         sentry_sdk.init(
             dsn=SENTRY_DSN,
-            # Add data like request headers and IP for users;
-            # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
             send_default_pii=True,
         )
     except BadDsn:
